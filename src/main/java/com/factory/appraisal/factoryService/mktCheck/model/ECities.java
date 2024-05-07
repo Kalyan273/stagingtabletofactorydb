7e874681329f66fcc78d522779c59f685d2fa6dc
rupesh.khade <rupesh.k@massiltech.com>
2024-05-07 11:01:02 +0530

    city entity added

---
+++ src/main/java/com/factory/appraisal/factoryService/mktCheck/model/ECities.java
@@ -0,0 +1,26 @@
+package com.factory.appraisal.factoryService.mktCheck.model;
+
+import com.factory.appraisal.factoryService.constants.AppraisalConstants;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "fl_city",schema = "marketcheck")
+@DynamicUpdate
+@DynamicInsert
+@Getter
+@Setter
+public class ECities {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "fl_city_id_seq")
+    @GenericGenerator(name = "fl_city_id_seq", strategy= AppraisalConstants.CUSTOM_SEQUENCE_GENERATOR)
+    private Long id;
+
+    private String name;
+
+}
